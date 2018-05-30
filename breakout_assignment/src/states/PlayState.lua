001d2d7bdc8a60739bb81b7575e97c875f8ab8f9
Jonathan Loy <jonrloy@gmail.com>
2018-05-30 15:33:08 -0400

    Fix recoverPoints returning to 5000 during ServeState

--- breakout_assignment/src/states/PlayState.lua
+++ breakout_assignment/src/states/PlayState.lua
@@ -29,7 +29,7 @@ function PlayState:enter(params)
     self.ball = params.ball
     self.level = params.level
 
-    self.recoverPoints = 5000
+    self.recoverPoints = params.recoverPoints
 
     -- give ball random starting velocity
     self.ball.dx = math.random(-200, 200)
